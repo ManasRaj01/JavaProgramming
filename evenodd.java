78306a8c7fc5bfd7a6b7f0a69b27c161cefc3692
ManasRaj01 <mansrajj.2005@gmail.com>
2026-07-16 21:10:15 +0530

    primecheck

--- evenodd.java
+++ evenodd.java
@@ -1,9 +1,14 @@
- class main{
+import java.util.Scanner;
 
-    public static void main (String[]args) {
+class Main {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
+
         System.out.println(n % 2 == 0 ? "Even" : "Odd");
 
+        sc.close();
     }
- }
+}
