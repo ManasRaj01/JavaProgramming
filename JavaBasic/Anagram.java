0f2791bae983da6d59ddcdc18543fb09dc4788b6
ManasRaj01 <manasrajj.2005@gmail.com>
2026-07-29 22:43:14 +0530

    Anagram

---
+++ JavaBasic/Anagram.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        //Conversion of string
+        }
+       
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        if(Arrays.equals(arr1, arr2)) {
+            System.out.println("Anagram");
+        } else {
+            System.out.println("Not Anagram");
+        }
+    }
+}
