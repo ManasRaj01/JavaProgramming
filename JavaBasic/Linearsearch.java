6d92489aa0a2a023374fc2b28259543dfcd4478f
ManasRaj01 <manasrajj.2005@gmail.com>
2026-08-03 22:40:00 +0530

    bubblesort

--- JavaBasic/Linearsearch.java
+++ JavaBasic/Linearsearch.java
@@ -3,16 +3,19 @@ class Linearsearch {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner (System.in);
+
         int n = sc.nextInt();
         int arr[] = new int[n]; 
         for(int i = 0; i<n; i++) 
-            arr [i] = sc.nextInt();
+
+       arr [i] = sc.nextInt();
         int key = sc.nextInt();
         boolean found = false;
         for(int num : arr) {
             if(num == key) {
                 found = true;
-                break;
+               
+             break;
                 
             }
         }
