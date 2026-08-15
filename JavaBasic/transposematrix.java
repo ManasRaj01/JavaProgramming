98ce9faf9f5dcaaac034d17074b5a688b6605981
ManasRaj01 <manasrajj.2005@gmail.com>
2026-08-15 22:10:24 +0530

    matrixadd

--- JavaBasic/transposematrix.java
+++ JavaBasic/transposematrix.java
@@ -3,15 +3,18 @@ public class transposematrix {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     
+
     int r = sc.nextInt(), c = sc.nextint();
+   
     
     int[][] a = new int [r][c];
+
 
     for(int i = 0; i<r; i++)
        for(int j = 0; j<c; j++)
     a[i][j] = sc.nextInt();
 
-
+    
     for(int j = 0; j<c; j++)
         for(int i = 0; i<r; i++)
     System.out.println(a[i][j]+ " ");
